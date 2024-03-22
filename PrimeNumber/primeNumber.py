bc062a19c09fff31d8d6ce62717abd6b72a2b3b1
Kiet Nguyen(Minor Account) <raki121004@gmail.com>
2024-03-22 10:38:46 +0700

    python

--- PrimeNumber/primeNumber.py
+++ PrimeNumber/primeNumber.py
@@ -1,17 +1,24 @@
-def primeNumber(n):
-    if n == 1:
-        print(f"{n} is not a prime number.")
-    elif n > 1:
-        for i in range(2, n):
-            if n % i == 0:
-                print(f"{n} is not a prime number.")
-                print(f"{i} times {n // i} = {n}")
-                break
-        else:
-            print(f"{n} is a prime number.")
-    else:
-        print(f"{n} is not a prime number.")
+def la_so_nguyen_to(n):
+    # Kiểm tra điều kiện ban đầu
+    # Nếu n nhỏ hơn hoặc bằng 1, hàm trả về False vì số
+    # 1 và các số âm không phải là số nguyên tố.
+    if n <= 1:
+        return False
+    # Kiểm tra từ 2 đến căn bậc hai của n
+    # Hàm duyệt qua các số từ 2 đến căn bậc hai của n (được làm tròn lên).
+    # Nếu n chia hết cho một số trong khoảng này (tức là n % i == 0),
+    # hàm trả về False vì n không phải là số nguyên tố.
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    # Trả về kết quả
+    # Nếu không tìm thấy số nào chia hết cho n, hàm trả về True, tức là n là số nguyên tố.
+    return True
 
 
-n = int(input("Enter an integer number: "))
-primeNumber(n)
+# Gọi hàm để kiểm tra một số ví dụ
+so_kiem_tra = int(input("Nhập số nguyên bất kỳ: "))
+if la_so_nguyen_to(so_kiem_tra):
+    print(f"{so_kiem_tra} là số nguyên tố.")
+else:
+    print(f"{so_kiem_tra} không là số nguyên tố.")
---
+++ SumOfTheDigitsInTheNumberSequence/Example.py
@@ -0,0 +1,6 @@
+numbers = input("Enter the series of numbers: ")
+sum = 0
+for digits in numbers:
+    sum += int(digits)
+
+print(f"Sum of the digits in the number sequence is: {sum}")
